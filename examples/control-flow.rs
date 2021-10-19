7d7a0a1b9cd02486211d842a68a2005239de5088
Benjamin Boboul <benjamin.boboul@protonmail.com>
2021-10-19 20:43:58 +0200

    Convert spaces to tabs

--- examples/control-flow.rs
+++ examples/control-flow.rs
@@ -1,4 +1,4 @@
-
+/* This file contain a collection of rust example from the official documentation */
 /* If, else if, else statement example */
 fn if_else_if_and_if() {
 	let number = 6;
@@ -16,64 +16,64 @@ fn if_else_if_and_if() {
 
 /* inline statement */
 fn inline_condition_statement() {
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
+	let condition = true;
+	let number = if condition { 5 } else { 6 };
 
-    println!("The value of number is: {}", number);
+	println!("The value of number is: {}", number);
 }
 
 /* infinite loop */
 fn infinite_loop() {
-    loop {
-        println!("again!");
-    }
+	loop {
+		println!("again!");
+	}
 }
 
 /* break from loop with a return value */
 fn finite_loop_with_return_value() {
-    let mut counter = 0;
+	let mut counter = 0;
 
-    let result = loop {
-        counter += 1;
+	let result = loop {
+		counter += 1;
 
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
+		if counter == 10 {
+			break counter * 2;
+		}
+	};
 
-    println!("The result is {}", result);
+	println!("The result is {}", result);
 }
 
 /* conditional loop with while */
 fn conditional_loop_with_while() {
-    let mut number = 3;
+	let mut number = 3;
 
-    while number != 0 {
-        println!("{}!", number);
+	while number != 0 {
+		println!("{}!", number);
 
-        number -= 1;
-    }
+		number -= 1;
+	}
 
-    println!("LIFTOFF!!!");
+	println!("LIFTOFF!!!");
 }
 
 /* for loop with while */
 fn for_loop_with_while() {
-    let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
+	let a = [10, 20, 30, 40, 50];
+	let mut index = 0;
 
-    while index < 5 {
-        println!("the value is: {}", a[index]);
+	while index < 5 {
+		println!("the value is: {}", a[index]);
 
-        index += 1;
-    }
+		index += 1;
+	}
 }
 
 /* for loop */
 fn for_loop() {
-    let a = [10, 20, 30, 40, 50];
+	let a = [10, 20, 30, 40, 50];
 
-    for element in a.iter() {
-        println!("the value is: {}", element);
-    }
+	for element in a.iter() {
+		println!("the value is: {}", element);
+	}
 }
