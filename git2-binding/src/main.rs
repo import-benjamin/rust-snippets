fd7a4ae52e41048e8522a65e21a5e77d1333adfc
Benjamin Boboul <8467845+benjaminBoboul@users.noreply.github.com>
2023-06-04 22:54:38 +0200

    feat(git2-binding): add peel_to_commit example

--- git2-binding/src/main.rs
+++ git2-binding/src/main.rs
@@ -8,4 +8,7 @@ fn main() {
     let head = repo.head().expect("Can't read HEAD.");
     
     println!("{}", head.name().unwrap());
+
+    let current_commit = head.peel_to_commit();
+    println!("Message: {}", current_commit.unwrap().message().unwrap());
 }
