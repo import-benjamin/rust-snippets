f8f91feb676af6851a12af2d887940e2fc39da7d
Benjamin Boboul <8467845+benjaminBoboul@users.noreply.github.com>
2023-06-17 23:48:06 +0200

    feat(georust): experiment boolean operations on polygons

--- georust-experiment/src/main.rs
+++ georust-experiment/src/main.rs
@@ -1,10 +1,19 @@
 use geo::geometry::{Coord, Rect};
+use geo::algorithm::BooleanOps;
 
 fn main() {
-    let rect = Rect::new(
+    let rect_a = Rect::new(
         Coord { x: 0., y: 4. },
         Coord { x: 3., y: 10. },
     );
 
-    println!("{rect:?}");
+    let rect_c = Rect::new(
+        Coord { x: 0., y: 4. },
+        Coord { x: 3., y: 10. },
+    );
+    
+    let rect_b = rect_a.to_polygon().difference(&rect_c.to_polygon());
+
+
+    println!("{rect_a:?}\n{rect_b:?}");
 }
